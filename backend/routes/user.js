2abf7d59c4930f8d5c091c806d7b6471b83e3b89
defineSenTinaL <defineSenTinaL>
2023-03-08 11:04:52 +0530

    Updated

--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const UserController = require('../controllers/UserController')
-
-router.post('/registration', UserController.registration)
-
-module.exports = router
